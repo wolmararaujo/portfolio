17261c7677651bd29f0bb6468f8c7fe6d5acbeae
Wolmar Araujo <wolmararaujo@gmail.com>
2021-08-25 08:05:08 -0300

    Estudos
    exercicios para codenapp.

---
+++ EstudosNCode/EstudosNCode/EstudosNCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EstudosNCode { class Program { static void Main(string[] args) 
+        { Console.WriteLine("--------------------------------------------------"); 
+            Console.WriteLine("Sequência Maneira!");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Entre com sua frase:"); string phrase = Console.ReadLine(); 
+            string[] words = phrase.Split(' ');
+            int aux = words.Length; for (int i = 0; i < aux; i++) 
+            { dynamic auxLetra = words.GetValue(0); 
+                bool result = words[i + 1].StartsWith(auxLetra); 
+                if (result) { Console.WriteLine("verdadeiro"); }
+                else { Console.WriteLine("falso"); } } } } }
